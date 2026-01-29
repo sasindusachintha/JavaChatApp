f793c6207684237203b4c49fba9e4a236ed4acce
Sasindu Sachintha <sasindusachintha1234@gmail.com>
2026-01-29 14:52:21 +0530

    Add multi-client chat support with Enter key sending in GUI

--- src/mychatapp/ChatClient.java
+++ src/mychatapp/ChatClient.java
@@ -1,4 +1,3 @@
-
 package mychatapp;
 
 import java.net.ServerSocket;
@@ -7,47 +6,47 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-        
+
 public class ChatClient2 {
-    public static void main(String[] args){
-        try{
-        Socket socket = new Socket("localhost", 5000);
-       
-        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader console = new BufferedReader( new InputStreamReader(System.in));
-         System.out.println("Enter your username: ");
-         String username = console.readLine();
-         writer.println(username);
-          
-        BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            
-            Thread readerThread = new Thread(() ->{
-                try{
+
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 5000);
+
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter your username: ");
+            String username = console.readLine();
+            writer.println(username);
+
+            BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            Thread readerThread = new Thread(() -> {
+                try {
                     String msg;
-                while((msg = serverReader.readLine()) != null){
-                System.out.println(msg);
-             }
-                }catch(IOException e){
-                   
+                    while ((msg = serverReader.readLine()) != null) {
+                        System.out.println(msg);
+                    }
+                } catch (IOException e) {
+
                 }
-             });
+            });
             readerThread.start();
-         String input;
-         while(true){
-            input = console.readLine();
-         
-             if(input.equalsIgnoreCase("exit")){
+            String input;
+            while (true) {
+                input = console.readLine();
+
+                if (input.equalsIgnoreCase("exit")) {
+                    writer.println(input);
+                    Thread.sleep(100);
+                    break;
+                }
                 writer.println(input);
-                Thread.sleep(100);
-                break;
-             }
-            writer.println(input);
-         }
-        socket.close();
-        
-        
-    }catch(IOException | InterruptedException e){
-        e.printStackTrace();
+            }
+            socket.close();
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-}
 }
--- src/mychatapp/ClientHandler.java
+++ src/mychatapp/ClientHandler.java
@@ -1,5 +1,5 @@
-
 package mychatapp;
+
 import java.net.Socket;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -10,109 +10,112 @@ import java.util.List;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-
 public class ClientHandler implements Runnable {
-     private Socket clientSocket;
-     private PrintWriter writer;
-     private String username;
-     
-     private static List<ClientHandler> clientList = new ArrayList<>();
-       
-     public ClientHandler(Socket socket){
-         this.clientSocket = socket;
-        
-     }     
-     @Override
-     public void run(){
-         try{
-         BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-         writer = new PrintWriter(clientSocket.getOutputStream(), true);
-         
-         username = reader.readLine();
-         
-         clientList.add(this);
-         
-         String joinMsg = getTimeStamp() +" " +username + " joined the chat!";
-         System.out.println( joinMsg) ;
-         broadcast (joinMsg);
-          
-         String message;
-         while((message = reader.readLine()) != null){
-             if(message.equalsIgnoreCase("exit")){
-                String leaveMsg = getTimeStamp() + " " + username + " left the chat!";
-                System.out.println(leaveMsg);
-                broadcastToAll(leaveMsg);
-                break;
-             }
-             
-             if (message.startsWith("/pm ")){
-                 handlePrivateMessage(message);
-                 continue;
-             }
-             
-             if(message.trim().isEmpty()){
-                 continue;
-             }
-             System.out.println(getTimeStamp() + " " + username +" says :" + message);
-             broadcast(getTimeStamp() + " " + username + ": " + message);
-         }
-         reader.close();
-         writer.close();
-         clientList.remove(this);
-         clientSocket.close();
-     }catch(IOException e){
-         e.printStackTrace();
-     }
-}
-      private void broadcast(String message){
-           for (ClientHandler client : clientList ){
-               if(client != this){
-               client.writer.println(message);
-           }
-           }
-      }
-      
-      private void broadcastToAll(String message){
-          for (ClientHandler client : clientList){
-              client.writer.println(message);
-          }
-      }
-      
-      private String getTimeStamp(){
-          LocalTime time = LocalTime.now();
-          DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-          return "[" + time.format(formatter) + "]";
-      }
-       
-      private ClientHandler getClientByUsername(String name){
-          for( ClientHandler client : clientList){
-              if(client.username.equalsIgnoreCase(name)){
-                  return client;
-              }
-          }
-          return null;
-      }
-      
-      private void handlePrivateMessage(String message){
-          String[] parts = message.split(" ", 3);
-          
-          if (parts.length > 3){
-              writer.println(" !! Usage: /pm username message");
-              return;
-          }
-          
-          String targetUsername = parts[1];
-          String privateMsg = parts[2];
-          
-          ClientHandler targetClient = getClientByUsername(targetUsername);
-          
-          if (targetClient == null){
-              writer.println("!! User " + targetUsername + "not found.");
-              return;
-          }
-          
-          String msg = getTimeStamp()+ " PM from " + username + ": " + privateMsg;
-          targetClient.writer.println(msg);
-      }
-      
+
+    private Socket clientSocket;
+    private PrintWriter writer;
+    private String username;
+
+    private static List<ClientHandler> clientList = new ArrayList<>();
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            writer = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            username = reader.readLine();
+
+            clientList.add(this);
+
+            String joinMsg = getTimeStamp() + " " + username + " joined the chat!";
+            System.out.println(joinMsg);
+            broadcast(joinMsg);
+
+            String message;
+            while ((message = reader.readLine()) != null) {
+                if (message.equalsIgnoreCase("exit")) {
+                    String leaveMsg = getTimeStamp() + " " + username + " left the chat!";
+                    System.out.println(leaveMsg);
+                    broadcastToAll(leaveMsg);
+                    break;
+                }
+
+                if (message.startsWith("/pm ")) {
+                    handlePrivateMessage(message);
+                    continue;
+                }
+
+                if (message.trim().isEmpty()) {
+                    continue;
+                }
+
+                System.out.println(getTimeStamp() + " " + username + " says :" + message);
+                broadcast(getTimeStamp() + " " + username + ": " + message);
+            }
+            reader.close();
+            writer.close();
+            clientList.remove(this);
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void broadcast(String message) {
+        for (ClientHandler client : clientList) {
+            
+                client.writer.println(message);
+            
+        }
+    }
+
+    private void broadcastToAll(String message) {
+        for (ClientHandler client : clientList) {
+            client.writer.println(message);
+        }
+    }
+
+    private String getTimeStamp() {
+        LocalTime time = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return "[" + time.format(formatter) + "]";
+    }
+
+    private ClientHandler getClientByUsername(String name) {
+        for (ClientHandler client : clientList) {
+            if (client.username.equalsIgnoreCase(name)) {
+                return client;
+            }
+        }
+        return null;
+    }
+
+    private void handlePrivateMessage(String message) {
+        String[] parts = message.split(" ", 3);
+
+        if (parts.length > 3) {
+            writer.println(" !! Usage: /pm username message");
+            return;
+        }
+
+        String targetUsername = parts[1];
+        String privateMsg = parts[2];
+
+        ClientHandler targetClient = getClientByUsername(targetUsername);
+
+        if (targetClient == null) {
+            writer.println("!! User " + targetUsername + "not found.");
+            return;
+        }
+
+        String msg = getTimeStamp() + " PM from " + username + ": " + privateMsg;
+        targetClient.writer.println(msg);
+    }
+
 }
